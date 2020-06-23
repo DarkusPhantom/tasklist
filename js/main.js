54de303fdc06f1b2726dd59c0da16531e254ab65
Darkus <luisfrodriguezb.lf@gmail.com>
2020-06-23 16:04:36 -0400

    Se creo el archivo js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function () {
+	//Variables
+	const menu = document.getElementById('menu');
+	const menuLateral = document.getElementById('menuLateral');
+
+	//EventListeners
+	menu.addEventListener("click", mostrarMenu);
+
+	//Funciones
+	function mostrarMenu(e) {
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+}());
