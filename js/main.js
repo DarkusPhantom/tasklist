b4723101cd1961a871870001caa1dde1d24b3ab0
Darkus <luisfrodriguezb.lf@gmail.com>
2020-07-03 11:43:19 -0400

    La funcion para la barra de navegacion del estado de Hoy, Semana y General, se ha reescrito

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
-//Variables
+(function () {
+	//------------------------Variables-----------------------
+	barra_calendario = document.querySelector('.nav_calendario');
 
-//EventListener
+	//----------------------EventListener----------------------
 
-//funciones
+	//Muestra el cual estado esta activo del calendario
+	barra_calendario.addEventListener('click', estadoActivo);
+
+	//-------------------------funciones-------------------
+
+	//Muestra cual estado esta activo en la barra de navegacion
+	function estadoActivo(e) {
+		e.preventDefault();
+		if(!e.target.classList.contains('active')) {
+			//Elimina los estados activo
+			for (var i = 0; i < barra_calendario.children.length; i++) {
+				if (barra_calendario != e.target) {
+					barra_calendario.children[i].classList.remove('active');
+				}
+			}
+			//Activa el estado seleccionado
+			e.target.classList.add('active');
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
